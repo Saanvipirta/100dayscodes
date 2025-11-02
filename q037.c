26b7586a68587f5d1fa5774e5057782cb9b0b2fe
Saanvipirta <saanvipirta@gmail.com>
2025-11-02 23:58:07 +0530

    Add files via upload

---
+++ q037.c
@@ -0,0 +1,33 @@
+//Write a program to find the LCM of two numbers.
+#include <stdio.h>
+
+
+int findHCF(int a, int b) {
+    while (b != 0) {
+        int temp = b;
+        b = a % b;
+        a = temp;
+    }
+    return a;
+}
+
+
+int findLCM(int a, int b) {
+    return (a * b) / findHCF(a, b);
+}
+
+int main() {
+    int num1, num2, lcm;
+
+    printf("Enter first number: ");
+    scanf("%d", &num1);
+
+    printf("Enter second number: ");
+    scanf("%d", &num2);
+
+    lcm = findLCM(num1, num2);
+
+    printf("The LCM of %d and %d is %d\n", num1, num2, lcm);
+
+    return 0;
+}
