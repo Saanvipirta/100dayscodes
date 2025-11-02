9b9bdb436c50378f0087cbf20f96ea30fb579cc2
Saanvipirta <saanvipirta@gmail.com>
2025-11-03 01:02:11 +0530

    Add files via upload

---
+++ q041.c
@@ -0,0 +1,26 @@
+//Write a program to swap the first and last digit of a number.
+#include <stdio.h>
+
+int main() {
+    int num, sum = 0;
+
+    printf("Enter a number: ");
+    scanf("%d", &num);
+
+    if (num <= 0) {
+        printf("Please enter a positive number.\n");
+        return 0;
+    }
+
+    for (int i = 1; i <= num / 2; i++) {
+        if (num % i == 0)
+            sum += i;
+    }
+
+    if (sum == num)
+        printf("%d is a Perfect Number.\n", num);
+    else
+        printf("%d is not a Perfect Number.\n", num);
+
+    return 0;
+}
