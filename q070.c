5bd0c9fbd044b521c7c344280125a6a5c3f0d5a7
Saanvipirta <saanvipirta@gmail.com>
2025-11-08 08:47:54 +0530

    Add files via upload

---
+++ q070.c
@@ -0,0 +1,40 @@
+//Rotate an array to the right by k positions.
+#include <stdio.h>
+
+int main() {
+    int arr[100], n, k, i;
+    int temp[100];
+
+    printf("Enter number of elements in the array: ");
+    scanf("%d", &n);
+
+    printf("Enter %d elements:\n", n);
+    for (i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
+    }
+
+    printf("Enter number of positions to rotate to the right: ");
+    scanf("%d", &k);
+
+    k = k % n;
+
+    for (i = 0; i < k; i++) {
+        temp[i] = arr[n - k + i];
+    }
+
+    for (i = n - 1; i >= k; i--) {
+        arr[i] = arr[i - k];
+    }
+
+    for (i = 0; i < k; i++) {
+        arr[i] = temp[i];
+    }
+
+    printf("Array after rotating right by %d positions:\n", k);
+    for (i = 0; i < n; i++) {
+        printf("%d ", arr[i]);
+    }
+
+    printf("\n");
+    return 0;
+}
