2b1785851426dbfe8709273ac669e8a07c075b77
Saanvipirta <saanvipirta@gmail.com>
2025-11-23 23:28:22 +0530

    Add files via upload

---
+++ q100.c
@@ -0,0 +1,25 @@
+//Print all sub-strings of a string.
+#include <stdio.h>
+#include <string.h>
+
+int main() {
+    char str[100];
+    printf("Enter a string: ");
+    scanf("%s", str);   
+
+    int len = strlen(str);
+
+    printf("All substrings:\n");
+
+    for (int i = 0; i < len; i++) {
+        for (int j = i; j < len; j++) {
+        
+            for (int k = i; k <= j; k++) {
+                putchar(str[k]);
+            }
+            printf("\n");
+        }
+    }
+
+    return 0;
+}
