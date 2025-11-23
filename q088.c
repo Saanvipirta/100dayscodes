3076dfe131fd5a2200cb6978630cc1313da22fde
Saanvipirta <saanvipirta@gmail.com>
2025-11-23 23:22:30 +0530

    Add files via upload

---
+++ q088.c
@@ -0,0 +1,14 @@
+//Replace spaces with hyphens in a string.
+#include <stdio.h>
+
+int main() {
+    char str[] = "Replace all spaces with hyphens";
+
+    for (int i = 0; str[i] != '\0'; i++) {
+        if (str[i] == ' ')
+            str[i] = '-';
+    }
+
+    printf("Result: %s\n", str);
+    return 0;
+}
