8abe278bab9f8d594f298a06d6b6b926d9f31aa3
Saanvipirta <saanvipirta@gmail.com>
2025-11-23 23:26:26 +0530

    Add files via upload

---
+++ q095.c
@@ -0,0 +1,31 @@
+//Check if one string is a rotation of another.
+#include <stdio.h>
+#include <string.h>
+
+int main() {
+    char s1[100], s2[100], temp[200];
+
+    printf("Enter first string: ");
+    fgets(s1, sizeof(s1), stdin);
+
+    printf("Enter second string: ");
+    fgets(s2, sizeof(s2), stdin);
+
+    s1[strcspn(s1, "\n")] = '\0';
+    s2[strcspn(s2, "\n")] = '\0';
+
+    if (strlen(s1) != strlen(s2)) {
+        printf("Not rotations\n");
+        return 0;
+    }
+
+    strcpy(temp, s1);
+    strcat(temp, s1);
+
+    if (strstr(temp, s2) != NULL)
+        printf("Strings are rotations\n");
+    else
+        printf("Not rotations\n");
+
+    return 0;
+}
